4fcc293b6f29186db3e9395f42569ab437bd5711
Daniel Buttars <dbuttars@asu.edu>
2016-02-13 13:28:24 -0700

    Added syscall_32.tbl which was modified to implement the syscall

--- my_syscall_test.c
+++ my_syscall_test.c
@@ -5,7 +5,7 @@
 int main()
 {
 //	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
-	syscall(__NR_my_syscall);
+	printf("%d\n", syscall(__NR_my_syscall));
 //	printf("%5s %-5s\t%8s %-10s\n", "1", "?", "00:00:31", "init");
 	return 0;
 }
