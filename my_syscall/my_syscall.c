6ca38e9fb6bf5dfb48f7ec51ee3bfd961b76982d
Daniel Buttars <dbuttars@asu.edu>
2016-02-14 18:10:24 -0700

    Fixed formatting and added comments to source code

--- my_syscall/my_syscall.c
+++ my_syscall/my_syscall.c
@@ -4,27 +4,36 @@
 #include<linux/sched.h> //Needed for the for_each_process() macro
 #include<linux/jiffies.h> //Needed to manage the time
 #include<asm/uaccess.h> //Needed to use copy_to_user
-#include<linux/slab.h> //Needed to use kmalloc
 
 struct task_struct *task; //To use the for_each_process macro
 
 asmlinkage long sys_my_syscall(int cap, int *to)
 {
-//	printk(KERN_INFO "This is the new system call Daniel Grant Burningham Buttars implemented.\n");
-	int buff[1024];
-	int len = 0;
-	for_each_process(task) {
-		printk("Task %s (pid = %d)\n", task->comm, task_pid_nr(task));
-		buff[len] = task_pid_nr(task);
-		printk("Task saved in array as: %u\n", buff[len]); 
-		len++;
+
+	int buff[1024]; 	// Our kernel space buffer, put data in here to be transferred to userspace
+	int len = 0; 		//Simple length int to keep track of how many procs we have
+
+
+	for_each_process(task) {	 	//Iterates through each process curently running (the current iteration is saved in the task
+						//struct and used for printing/saving 
+
+		printk("Task %s (pid = %d)\n", task->comm, task_pid_nr(task)); 	//Print out the task info to the kern.log (TO BE REMOVED)
+
+		buff[len] = task_pid_nr(task);					//Save the task PID into the buffer
+
+		printk("Task saved in array as: %u\n", buff[len]); 		//Print out the saved num, for debugging purposes (TO BE REMOVED) 
+
+		len++; 								//Iterate our length int
 	}	
 
+	//If our length is bigger than the cap set by the user
+	//program then reduce the length
 	if (len > cap) len = cap;
-	printk("Len is %d\n", len);
-	int size;
-	size = len * 4;	
-	return copy_to_user(to, buff, size); 
+
+	printk("Len is %d\n", len);		//Print out the len, for debugging purposes (TO BE REMOVED)
+	int size; 				//Easy way to calc the size for the copy_to_user function
+	size = len * 4; 			//Each int takes up 4 bytes of memory	
+	return copy_to_user(to, buff, size); 	//Copy the (size) number of bytes from (buff) to (to) 
 }
 
 /*---End of my_syscall.c---*/
--- my_syscall_test.c
+++ my_syscall_test.c
@@ -4,25 +4,23 @@
 
 int main()
 {
+//	Saved for formatting, as it is similar to the formatting used by
+//	the ps -e call
 //	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
 //	printf("%d\n", syscall(__NR_my_syscall));
 //	printf("%5s %-5s\t%8s %-10s\n", "1", "?", "00:00:31", "init");
 	
+	int buff[1024]; 				//Initialize our data buffer for the PID data
+	syscall(__NR_my_syscall, sizeof(buff), buff);	//Call the syscall, which should copy over the
+							//PID data to our buffer
 	
-//	for (i = 0; i < 1000; i++) {
-//		array[i] = 0;
-//	}	
-	
-//	for (i = 0; i < 1000; i++) {
-//		printf("PID: %d\n", array[i]);
-//	}	
-	int buff[1024];
-	syscall(__NR_my_syscall, sizeof(buff), buff);
-	
-	int i;
-	for (i = 0; i < 256; i++){
-		printf("PID: %d\n", buff[i]); 
-		if (buff[(i+1)] == 0) i = 256;
+	int i; //Simple iteration int
+	for (i = 0; i < 1024; i++){ 	//There are a maximum of 1024 ints in the buffer so don't try to
+					//read past that
+						
+		printf("PID: %d\n", buff[i]); 	//Print out the PID from the buffer 
+		if (buff[(i+1)] == 0) i = 1024; 	//Stop the looping if the PID of the next value in the buff
+						//is 0 (means the rest of the buffer is empty)
 	}
 
 	return 0;
