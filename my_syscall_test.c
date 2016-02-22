e13d9da02d4d174eee66989a3ef1553fc7497900
dur1n <tqtuan1982@gmail.com>
2016-02-22 05:12:35 -0700

    Update my_syscall_test.c
    used two syscalls

--- my_syscall_test.c
+++ my_syscall_test.c
@@ -1,27 +1,31 @@
 #include <linux/unistd.h>
 #include <stdio.h>
-#define __NR_my_syscall 359
+#define __NR_my_syscall_num 359
+#define __NR_my_syscall 360
+struct buff_struct {
+ 	int pid;
+	char comm[64];
+};
 
-int main()
-{
+int main(){
 //	Saved for formatting, as it is similar to the formatting used by
 //	the ps -e call
 //	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
 //	printf("%d\n", syscall(__NR_my_syscall));
 //	printf("%5s %-5s\t%8s %-10s\n", "1", "?", "00:00:31", "init");
 	
-	int buff[1024]; 				//Initialize our data buffer for the PID data
-	syscall(__NR_my_syscall, sizeof(buff), buff);	//Call the syscall, which should copy over the
-							//PID data to our buffer
-	
-	int i; //Simple iteration int
-	for (i = 0; i < 1024; i++){ 	//There are a maximum of 1024 ints in the buffer so don't try to
-					//read past that
-						
-		printf("PID: %d\n", buff[i]); 	//Print out the PID from the buffer 
-		if (buff[(i+1)] == 0) i = 1024; 	//Stop the looping if the PID of the next value in the buff
-						//is 0 (means the rest of the buffer is empty)
+	// 1.step get total number of taks
+	int tasks_num;
+	tasks_num = syscall(__NR_my_syscall_num);
+	printf("Task_num:%u\n",tasks_num ); 	
+			
+	// 2.getting data-fields from kernel-space
+	struct buff_struct buff[2000];
+	syscall(__NR_my_syscall, tasks_num, sizeof(buff), buff);
+	int i;
+	for (i = 0; i < tasks_num; i++){ 
+		printf("PID: %u\n", buff[i].pid ); 	
+		printf("NAME: %s\n", buff[i].comm ); 				
 	}
-
 	return 0;
 }
