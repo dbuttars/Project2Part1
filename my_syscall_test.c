fe0b0f45a438db0795914771171ce829dd12f58d
dur1n <tqtuan1982@gmail.com>
2016-02-23 18:00:52 -0700

    Update my_syscall_test.c

--- my_syscall_test.c
+++ my_syscall_test.c
@@ -1,31 +1,26 @@
 #include <linux/unistd.h>
 #include <stdio.h>
-#define __NR_my_syscall_num 359
-#define __NR_my_syscall 360
+#define __NR_my_syscall 359
 struct buff_struct {
  	int pid;
-	char comm[64];
+	char comm[16];
+	char tty[10];
+ 	unsigned long time;
 };
-
 int main(){
-//	Saved for formatting, as it is similar to the formatting used by
-//	the ps -e call
-//	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
-//	printf("%d\n", syscall(__NR_my_syscall));
 //	printf("%5s %-5s\t%8s %-10s\n", "1", "?", "00:00:31", "init");
 	
-	// 1.step get total number of taks
-	int tasks_num;
-	tasks_num = syscall(__NR_my_syscall_num);
-	printf("Task_num:%u\n",tasks_num ); 	
-			
-	// 2.getting data-fields from kernel-space
-	struct buff_struct buff[2000];
-	syscall(__NR_my_syscall, tasks_num, sizeof(buff), buff);
+	int tasks_num ;
 	int i;
-	for (i = 0; i < tasks_num; i++){ 
-		printf("PID: %u\n", buff[i].pid ); 	
-		printf("NAME: %s\n", buff[i].comm ); 				
+	// getting data-fields from kernel-space
+	struct buff_struct buff[500];
+	tasks_num = syscall(__NR_my_syscall, sizeof(buff), buff);
+	// iterate through struct array and display
+	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
+	for (i = 0; i < tasks_num; i++){
+	//printf("%5d %-5s\t%7s %-10s\n",buff[i].pid, "?","00:00:31", buff[i].comm);	
+	//printf("%5d %-5s\t%7s %-10s\n",buff[i].pid, buff[i].tty,"00:00:31", buff[i].comm);
+printf("%5d %-5s\t%lu %-10s\n",buff[i].pid, buff[i].tty,buff[i].time,buff[i].comm);			
 	}
 	return 0;
 }
