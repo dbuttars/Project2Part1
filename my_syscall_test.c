5e0e4b1f8713aa198c3a58736c5276dc07dbe0e8
dur1n <tqtuan1982@gmail.com>
2016-03-01 21:49:22 -0700

    Update my_syscall_test.c

--- my_syscall_test.c
+++ my_syscall_test.c
@@ -16,11 +16,11 @@ int main(){
 	struct buff_struct buff[500];
 	tasks_num = syscall(__NR_my_syscall, sizeof(buff), buff);
 	// iterate through struct array and display
-	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
+	printf("%5s %-5s\t%6s %-10s\n", "PID", "TTY", "TIME", "CMD");
 	for (i = 0; i < tasks_num; i++){
 	//printf("%5d %-5s\t%7s %-10s\n",buff[i].pid, "?","00:00:31", buff[i].comm);	
 	//printf("%5d %-5s\t%7s %-10s\n",buff[i].pid, buff[i].tty,"00:00:31", buff[i].comm);
-printf("%5d %-5s\t%lu %-10s\n",buff[i].pid, buff[i].tty,buff[i].time,buff[i].comm);			
+printf("%5d %-5s\t%8lu %-10s\n",buff[i].pid, buff[i].tty,buff[i].time,buff[i].comm);			
 	}
 	return 0;
 }
