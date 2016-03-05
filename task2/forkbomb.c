746d06a81a7c34315c8e0277519722af9c4c439d
dur1n <tqtuan1982@gmail.com>
2016-03-05 16:28:57 -0700

    Update forkbomb.c

--- task2/forkbomb.c
+++ task2/forkbomb.c
@@ -5,27 +5,30 @@
 #include <linux/signal.h>
 #include <linux/delay.h>
 #include <linux/list.h>
+#include <linux/kthread.h>
+
 struct task_struct *task;
 
 // thread function from pdf slide
 int my_kthread_function(void* data){
 	while(!kthread_should_stop()){
 		struct list_head *p; // part od sched.h
-		struct task_struct newTask;
+		struct task_struct tsk;
 
 		msleep(1000);	// most of the time the thread sleeps
 				// check every second
-		read_lock(&tasklist_lock); // Take tasklist_lock before you iterate each process.
-		
+		// read_lock(&tasklist_lock); // Take tasklist_lock before you iterate each process.
+		rcu_read_lock();
 		for_each_process(task){
-			list_for_each(p, &task->children){ 	
-				newTask = *list_entry(p, struct task_struct, sibling); //check each child
-				//*8 
+			list_for_each(p, &(task->children)){ 	
+				tsk = *list_entry(p, struct task_struct, sibling); //check each child
+				//** do 
 			}
 		}
-		read_unlock(&tasklist_lock);//Release tasklist_lock after you are done.
-		
-		oom_kill_process();
+		//read_unlock(&tasklist_lock);//Release tasklist_lock after you are done.
+		rcu_read_unlock();
+
+		oom_kill_process(p);
 	}
 	return 0;
 }
@@ -54,7 +57,7 @@ static int __init forkbomb(void){
 // ************* module end here
 static void __exit forkbomb_exit(void){
 	// stop thread in the end
-	kthread_stop(task);
+	// int kthread_stop(task);
 }
 
 module_init(forkbomb);
