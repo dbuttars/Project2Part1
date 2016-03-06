84c223d5f77c246fa911cd896f3611da90827ea9
dur1n <tqtuan1982@gmail.com>
2016-03-06 04:48:09 -0700

    Update my_syscall_test.c

--- my_syscall_test.c
+++ my_syscall_test.c
@@ -1,27 +1,25 @@
 #include <linux/unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #define __NR_my_syscall 359
+#define __NR_process_count 360 
 
-int main()
-{
-//	Saved for formatting, as it is similar to the formatting used by
-//	the ps -e call
-//	printf("%5s %-5s\t%8s %-10s\n", "PID", "TTY", "TIME", "CMD");
-//	printf("%d\n", syscall(__NR_my_syscall));
-//	printf("%5s %-5s\t%8s %-10s\n", "1", "?", "00:00:31", "init");
-	
-	int buff[1024]; 				//Initialize our data buffer for the PID data
-	syscall(__NR_my_syscall, sizeof(buff), buff);	//Call the syscall, which should copy over the
-							//PID data to our buffer
-	
-	int i; //Simple iteration int
-	for (i = 0; i < 1024; i++){ 	//There are a maximum of 1024 ints in the buffer so don't try to
-					//read past that
-						
-		printf("PID: %d\n", buff[i]); 	//Print out the PID from the buffer 
-		if (buff[(i+1)] == 0) i = 1024; 	//Stop the looping if the PID of the next value in the buff
-						//is 0 (means the rest of the buffer is empty)
-	}
+struct buff_struct {
+ 	int pid;
+	char comm[16];
+	char tty[10];
+	char time[9];
+} *buff;
 
+int main () {		
+	long tasks_num;
+	tasks_num = syscall(__NR_process_count);
+	printf("Number of tasks is: %ld\n", tasks_num);	
+	buff = malloc(sizeof(struct buff_struct) * tasks_num);
+	tasks_num = syscall(__NR_my_syscall, sizeof(buff), buff);	
+	int i;
+	for (i = 0; i < tasks_num; i++) {
+		printf("%5d %-5s %s %-10s\n",buff[i].pid, buff[i].tty,buff[i].time,buff[i].comm);			
+	}	
 	return 0;
 }
